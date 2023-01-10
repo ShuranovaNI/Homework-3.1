baa808626a16d8b19f94221365f137214696bac5
Shuranova N <ni.shuranova@gmail.com>
2023-01-10 17:25:07 +0300

    Сorrection

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
-    int cost;
-    int costOneMiles = 20;
-    public int calculate (int cost) {
-         int miles = cost / costOneMiles;
+
+    public int calculate(int cost) {
+        int costOneMiles = 20;
+        int miles = cost / costOneMiles;
         return miles;
     }
 
